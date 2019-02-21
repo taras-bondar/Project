09004fef70ae643f95eec1873a3cd476181c014a
Nikita Parkhomenko <nekit.park2019@gmail.com>
2019-02-21 18:15:49 +0200

    add logo Ham

--- js/work-filter.js
+++ js/work-filter.js
@@ -1,6 +1,9 @@
-let workBtnList = document.querySelector('.work-example');
-workBtnList.addEventListener('click', function() {
-  let className = this.getAttribute('data-target');
- console.log(className);
+// let workBtnList = document.querySelector('.work-example');
+// workBtnList.addEventListener('click', function() {
+//   let className = this.getAttribute('data-target');
+//  console.log(className);
 
-});
+// });
+const bodyNode = document.body.lastChild;
+console.log(bodyNode);
+
